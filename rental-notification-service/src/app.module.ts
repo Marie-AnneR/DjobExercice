0db4fb4081b4e0c8699f065d94a60523ddbbe6cc
Marie-Anne <marie-anne.roth@epitech.eu>
2024-12-18 10:15:07 +0100

    pushing functionnal backend

--- rental-notification-service/src/app.module.ts
+++ rental-notification-service/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleModule } from '@nestjs/schedule';
-import { Rental } from './rental/rental.entity';
+import { Rental } from './entities/rental.entity';
 import { Customer } from './entities/customer.entity';
 import { RentalController } from './rental/rental.controller';
 import { RentalService } from './rental/rental.service';
 import { NotificationService } from './notification/notification.service';
+import { NotificationController } from './notification/notification.controller';
 
 @Module({
   imports: [
@@ -22,7 +23,7 @@ import { NotificationService } from './notification/notification.service';
     TypeOrmModule.forFeature([Rental, Customer]),
     ScheduleModule.forRoot(),
   ],
-  controllers: [RentalController],
+  controllers: [RentalController, NotificationController],
   providers: [RentalService, NotificationService],
 })
 export class AppModule {}
